0948ee674eec9ae320344650cd1134a144d1edf8
Aleksey Danchenko <aleksey.danchenko@gmail.com>
2018-12-08 22:19:12 +0200

    Messages are sent efficiently with Stripo template

--- 13/index.js
+++ 13/index.js
@@ -20,13 +20,41 @@ fs.appendFile('text.txt','/n'+some_text, function(err){
 var pass = require('./pass.js');
 var password = pass.password;
 var account = pass.account;
-console.log(password);
-console.log(account);
-
+var recepient = pass.recepient;
 
 //Sending an email
-//var nodemailer = require('nodemailer');
-//sdfsdfdsfsdf
-//var transporter = nodemailer.createTransport();
-console.log(password);
-console.log(account);
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+service: 'gmail',
+auth:{
+    user:account,
+    pass:password
+}
+});
+var template = fs.readFileSync('./template.html', 'utf8', function(err,data){
+
+    if (err) console.log(err);
+    return data;
+});
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: recepient,
+        subject: 'Привет мир от NodeJS ✔', // Subject line
+        text: 'шаблонный текст? беее... смотри HTML!', // plain text body
+        html: template // html body
+    };
+
+//    console.log(template);
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+
+    });
+
+
